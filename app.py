6ef8ea9aba8d52c69fdaa891849b9770fe31a8b9
Mai Van Do <minty@Mintys-MBP.lan>
2026-04-23 03:21:38 +0800

    fixing bugs so that flask runs both login.html and signup.html

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 
 app.run(debug=True)
