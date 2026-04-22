477491154cec2ae87f5f241ca81287b92db27020
Mai Van Do <minty@Mintys-MBP.lan>
2026-04-23 02:38:27 +0800

    adding register.hmtl to flask

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/register')
 def register():
     return render_template('register.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
